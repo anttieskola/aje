b2ea2478ee83ba9a2a65834b484434046612532b
Antti Eskola <anttieskola@users.noreply.github.com>
2023-10-05 01:48:00 +0300

    fix: index search (added ? to escaped characters)

--- src/Application/RedisTokenizer.cs
+++ src/Application/RedisTokenizer.cs
@@ -2,9 +2,10 @@
 
 public static class RedisTokenizer
 {
+    // tested with ft.search / ft.index
     // https://redis.io/docs/interact/search-and-query/advanced-concepts/escaping/
-    // + '/'
-    private const string _escapeCharacters = ",.<>{}[]\"':;!@#$%^&*()-+=~/";
+    // + '/' + '?'
+    private const string _escapeCharacters = ",.<>{}[]\"':;!@#$%^&*()-+=~/?";
 
     public static string RedisEscape(this string str)
     {
