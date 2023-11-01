cccebfc696f6c9d13cfaf3081073309065a2878e
Antti Eskola <anttieskola@users.noreply.github.com>
2023-11-01 14:02:54 +0200

    landing page, chat creation to chat (design not functional)

---
+++ src/Domain/Entities/AiChat.cs
@@ -0,0 +1,20 @@
+﻿namespace AJE.Domain.Entities;
+
+public record AiChatHistory
+{
+    public required Guid Id { get; init; }
+    public EquatableList<AiChatMessage> Messages { get; init; } = new();
+}
+
+public enum AiChatRole
+{
+    User = 1,
+    Bot = 2
+}
+
+public record AiChatMessage
+{
+    public AiChatRole Role { get; init; }
+    public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+    public required string Message { get; init; }
+}
