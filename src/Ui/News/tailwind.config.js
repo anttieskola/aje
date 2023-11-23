6c37480501413adefd72cc2769973c3b5a93e7ca
Antti Eskola <anttieskola@users.noreply.github.com>
2023-11-23 16:30:40 +0200

    same screen definitions from antai to news

--- src/Ui/News/tailwind.config.js
+++ src/Ui/News/tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
         './**/*.{razor,html,cshtml}'
     ],
     theme: {
+        screens: {
+            'xs': '400px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1000px',
+            'xl': '1400px',
+            '2xl': '2000px',
+        },
         extend: {},
     },
     plugins: [],
