188c5c388eb062014c1782be9182bbe2becc438e
Antti Eskola <anttieskola@users.noreply.github.com>
2023-11-24 20:27:19 +0200

    fix

--- src/Ui/Antai/Shared/InteractionCreator.razor.js
+++ src/Ui/Antai/Shared/InteractionCreator.razor.js
@@ -4,25 +4,3 @@ export function focusElement(elementId) {
         element.focus();
     }
 }
-
-var outputStarted = false;
-
-export function outputStart() {
-    outputStarted = true;
-    setTimeout(() => {
-        scrollToBottom();
-    }, 100);
-}
-
-export function outputEnd() {
-    outputStarted = false;
-}
-
-export function scrollToBottom() {
-    if (!outputStarted) {
-        window.scrollTo(0, document.body.scrollHeight);
-        setTimeout(() => {
-            scrollToBottom();
-        }, 100);
-    }
-}
