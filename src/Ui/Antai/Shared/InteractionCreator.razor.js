ecf185a0b90f65c2298339747fcea678a81ff8ba
Antti Eskola <anttieskola@users.noreply.github.com>
2023-11-24 16:19:41 +0200

    AiChat message "bubles" dynamic size improvements

--- src/Ui/Antai/Shared/InteractionCreator.razor.js
+++ src/Ui/Antai/Shared/InteractionCreator.razor.js
@@ -2,14 +2,12 @@ export function focusElement(elementId) {
     var element = document.getElementById(elementId);
     if (element) {
         element.focus();
-        console.log("focusElement: " + elementId);
     }
 }
 
 var outputStarted = false;
 
 export function outputStart() {
-    console.log("outputStart");
     outputStarted = true;
     setTimeout(() => {
         scrollToBottom();
@@ -17,12 +15,10 @@ export function outputStart() {
 }
 
 export function outputEnd() {
-    console.log("outputEnd");
     outputStarted = false;
 }
 
 export function scrollToBottom() {
-    console.log("scrollToBottom");
     if (!outputStarted) {
         window.scrollTo(0, document.body.scrollHeight);
         setTimeout(() => {
