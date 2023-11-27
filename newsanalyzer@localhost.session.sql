e0a66cc838bacfdd250def91ae07cdef1845e63b
Antti Eskola <anttieskola@users.noreply.github.com>
2023-11-27 16:09:54 +0200

    fresh backups

--- newsanalyzer@localhost.session.sql
+++ newsanalyzer@localhost.session.sql
@@ -1,4 +1,13 @@
-select sp.source
-from sentimentpolarities sp
-group by sp.source
-HAVING count(*) > 1;
+-- select sp.source
+-- from sentimentpolarities sp
+-- group by sp.source
+-- HAVING count(*) > 1;
+
+select * from sentimentpolarities order by serial desc;
+
+-- SELECT column_name, data_type, character_maximum_length
+-- FROM information_schema.columns
+-- WHERE table_name = 'sentimentpolarities';
+
+-- set serial to max value (after restore/copy of data)
+-- SELECT setval('sentimentpolarities_serial_seq', (SELECT MAX(serial) FROM sentimentpolarities));
