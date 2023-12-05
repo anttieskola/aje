39f0f33fb1cc8ca5554d30cb32e7775c76d37459
Antti Eskola <anttieskola@users.noreply.github.com>
2023-12-05 21:38:23 +0200

    tablet test 2

--- src/Ui/News/tailwind.config.js
+++ src/Ui/News/tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         screens: {
             'xs': '400px',
             'sm': '640px',
-            'md': '748px',
+            'md': '700px',
             'lg': '1000px',
             'xl': '1400px',
             '2xl': '2000px',
