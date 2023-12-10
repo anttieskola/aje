59594c9695cb4c2f874e1d58bf176fe6f7c6cdd2
Antti Eskola <anttieskola@users.noreply.github.com>
2023-12-11 01:15:25 +0200

    Added token counter to promptstudio

---
+++ src/Domain/Queries/TokenizeContentQuery.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AJE.Domain.Queries;
+
+public record TokenizeContentQuery : IRequest<TokenizeResponse>
+{
+    public required string Content { get; init; }
+}
+
+public class TokenizeContentQueryHandler : IRequestHandler<TokenizeContentQuery, TokenizeResponse>
+{
+    private readonly IAiModel _aiModel;
+
+    public TokenizeContentQueryHandler(
+        IAiModel aiModel)
+    {
+        _aiModel = aiModel;
+    }
+
+    public Task<TokenizeResponse> Handle(TokenizeContentQuery request, CancellationToken cancellationToken)
+    {
+        return _aiModel.TokenizeAsync(new TokenizeRequest
+        {
+            Content = request.Content
+        }, cancellationToken);
+    }
+}
